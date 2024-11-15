ee0f76140017dc5dce4da2afb1ae81eb955a790a
Usman Rajpoot <usmanarif534@gmail.com>
2024-11-15 15:18:27 +0500

    Feat: Add app Theme (Dark, Light) & Colors.

---
+++ lib/core/config/theme/app.theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/config/theme/app.colors.dart';
+
+abstract class AppTheme {
+  ///## Light Theme
+  ///
+  ///Custom designed theme for light mode.
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+
+  ///## Dark Theme
+  ///
+  ///Custom designed theme for dark mode.
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/config/theme/app.theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Spotify Clone',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       home: Container(),
     );
   }
